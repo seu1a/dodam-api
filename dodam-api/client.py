b2635173cadfc71ca9904878ce98e9305761ce29
이슬아 <rheeseula@gmail.com>
2024-09-23 19:40:30 +0900

    feat: retry when access_token expired

--- dodam-api/client.py
+++ dodam-api/client.py
@@ -1,14 +1,29 @@
 import requests
 from utils import Utils
+from apis import auth
+from error import TokenExpiredException
 
 class Client() :
     def __init__(self) :
         self.utils = Utils()
     
     def request(self, url, method, **kwargs) :
-        return requests.request(
-            method,
-            url,
-            headers=kwargs.get("headers", {}),
-            json=kwargs.get("body", {})).json()["data"]
-        
+        while True:
+            try :
+                res = requests.request(
+                    method,
+                    url,
+                    headers=kwargs.get("header", {}),
+                    json=kwargs.get("body", {}),
+                    params=kwargs.get("param", {})
+                )
+                
+                if (res.status_code == 401) :
+                    raise TokenExpiredException
+                
+                return res.json()["data"]
+
+            except (TokenExpiredException) :
+                auth.reissue(self, self.utils)
+                continue
+            
